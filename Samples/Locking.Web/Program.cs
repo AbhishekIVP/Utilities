3351f2405b68e4cd05d898bae09b2a90f86e024f
spanhotra <79205065+spanhotra@users.noreply.github.com>
2023-01-29 01:56:52 +0530

    Added a Common Extension Library for Distributed Locking

--- Samples/Locking.Web/Program.cs
+++ Samples/Locking.Web/Program.cs
@@ -1,9 +1,8 @@
-using System.Collections.Immutable;
-using Medallion.Threading;
-using Medallion.Threading.Redis;
-using StackExchange.Redis;
+using ivp.edm.distributedlock;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.AddDynamicRedisLocking();
 
 // Add services to the container.
 
@@ -11,16 +10,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-ConfigurationOptions _options = new ConfigurationOptions()
-{
-    Password = builder.Configuration["DistributedLocking:Redis:Password:Value"],
-    DefaultDatabase = Convert.ToInt32(builder.Configuration["DistributedLocking:Redis:Database"])
-};
-_options.EndPoints.Add(builder.Configuration["DistributedLocking:Redis:Endpoint"]);
-var connection = await ConnectionMultiplexer.ConnectAsync(_options); // uses StackExchange.Redis
-
-builder.Services.AddSingleton<IDistributedLockProvider>(_ => new RedisDistributedSynchronizationProvider(connection.GetDatabase()));
 
 var app = builder.Build();
 
