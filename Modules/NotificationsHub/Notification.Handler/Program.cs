a90354858bcf49c1d7440967d1edbd8224583296
spanhotra <79205065+spanhotra@users.noreply.github.com>
2023-02-08 14:15:08 +0530

    Moved sender to correct location

--- Modules/NotificationsHub/Notification.Handler/Program.cs
+++ Modules/NotificationsHub/Notification.Handler/Program.cs
@@ -4,26 +4,13 @@ using ivp.edm.notification.template.implementation;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.AddExtensions(Enable.PUB_SUB | Enable.SECRETS);
+
+builder.AddExtensions(Enable.PUB_SUB | Enable.SECRETS | Enable.APM | Enable.LOG);
 
 builder.Services.AddNotifications(builder.Configuration);
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseAuthorization();
 
 app.StartSubscriber(_ =>
 {
